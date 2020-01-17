b5dc8e3f799bb249a5498c152c80d322c4a8b824
Aayush soni <A-a-y-u-s-h-s-o-n-i@github.com>
2020-01-17 19:59:53 +0530

    Updates

---
+++ precache-manifest.b83f2b2390e44370c91dc039aee94c22.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5848936a6c2fe99c065341b824795ba8",
+    "url": "/Movies/index.html"
+  },
+  {
+    "revision": "6e5175cf22fa5d4a49c6",
+    "url": "/Movies/static/css/2.cb54562d.chunk.css"
+  },
+  {
+    "revision": "d53964f48fe2dbe24a54",
+    "url": "/Movies/static/css/main.c5de0c4f.chunk.css"
+  },
+  {
+    "revision": "6e5175cf22fa5d4a49c6",
+    "url": "/Movies/static/js/2.074a38af.chunk.js"
+  },
+  {
+    "revision": "74dd6f9d1a5d80dce8b8fef0e72e38d0",
+    "url": "/Movies/static/js/2.074a38af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d53964f48fe2dbe24a54",
+    "url": "/Movies/static/js/main.3453b092.chunk.js"
+  },
+  {
+    "revision": "83ffeed3d54b09ff4e09",
+    "url": "/Movies/static/js/runtime-main.a25ca099.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Movies/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Movies/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Movies/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Movies/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Movies/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
